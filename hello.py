ecbd1395e563edf0133619f8841695f7c0b0433c
Ron Wolf <ron-wolf@users.noreply.github.com>
2020-12-03 19:56:08 -0500

    Call-by-name: get sys.argv
    I'm not sure if this is the way to do it :/

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-print("Hello, world!")
-print("Goodbye, world!")
+import sys
+
+if len(sys.argv > 0):
+	name = sys.argv[0]
+	print("Hello, ", name)
+	print("Goodbye, ", name)
+else:
+	print("Hello, world!")
+	print("Goodbye, world!")
